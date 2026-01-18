42814f43eba987cddbf36fe4860c62bdb8ca7170
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2026-01-18 10:13:58 -0300

    feat: Adiciona validade para o cpf

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -7,4 +7,12 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
 
+    def validate_cpf(self, cpf):
+        """Verifica se o cpf tem 11 digitos e se é numérico"""
+        if len(cpf) != 11:
+            raise serializers.ValidationError('O cpf deve ter exatamente 11 digitos')
         
+        if not cpf.isdigit():
+            raise serializers.ValidationError('O cpf deve conter apenas números')
+
+        return cpf
